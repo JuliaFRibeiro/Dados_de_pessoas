988e137f4bff1ebcfe9dd467205391559d94b5f3
Julia <julia_felix03@hotmail.com>
2021-09-01 20:49:56 -0300

    Adicionar arquivos de projeto.

---
+++ Dados_de_pessoas/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dados_de_pessoas
+{
+    class Pessoa
+    {
+        //declarando variavel idade
+        public int idade;
+        //declarando variavel nome
+        public string nome;
+    }
+}
---
+++ Dados_de_pessoas/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Dados_de_pessoas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaração de variavel da classe
+            Pessoa A;
+            Pessoa B;
+
+            //isntanciar a variavel
+            A = new Pessoa();
+            B = new Pessoa();
+
+            //info para o meu usuario
+            Console.WriteLine("Informe o nome da pessoa A");
+            A.nome = Console.ReadLine();
+            Console.WriteLine("Informe a idade da pessoa A");
+            A.idade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Informe o nome da pessoa B");
+            B.nome = Console.ReadLine();
+            Console.WriteLine("Informe aidade da passoa B");
+            B.idade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //comparando a idade
+            if(A.idade>B.idade)
+            {
+                Console.WriteLine("Pessoa mais velha:" + A.nome);
+            }
+            else
+            {
+                Console.WriteLine("Pessoa mais velha:" + B.nome);
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
